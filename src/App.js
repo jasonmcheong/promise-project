804a284aeee54008abbcee347f90b82602a5bc37
Jason Cheong <jasonmcheong@gmail.com>
2019-05-03 22:19:41 -0600

    retrieving data from Wordpress REST API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Carousel from './components/Carousel.js';
 
-const App = () => {
-    return <div className="App">App Component</div>;
-};
+class App extends React.Component {
+    state = {
+        questions: [],
+    };
+
+    componentWillMount = () => {
+        fetch('http://ea-mondo.org/wp-json/wp/v2/promise_questions')
+            .then(res => res.json())
+            .then(resArr => {
+                resArr.map(res => this.setState({ questions: res.acf.promise_questions }));
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.questions.length > 0 ? <Carousel questions={this.state.questions} /> : <h1>Loading</h1>}
+            </div>
+        );
+    }
+}
 
 export default App;
