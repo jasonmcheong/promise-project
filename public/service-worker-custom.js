2a82a738d68fb9bf571fdb7d97cba7d32c0e7149
Jason Cheong <jasonmcheong@gmail.com>
2019-05-28 16:42:39 -0600

    successfully retrieving the results from IndexedDB

--- public/service-worker-custom.js
+++ public/service-worker-custom.js
@@ -1,70 +1,68 @@
 // IndexedDB
 // ** IndexedDB version is v10 **
-const request = indexedDB.open('questionsDB', 10);
-
-var db;
+let request = indexedDB.open('questionsDB', 10);
 
 request.onerror = function(error) {
-    console.log(error);
-};
-
-request.onupgradeneeded = function(e) {
-    console.log('Upgrading...');
-    var thisDB = e.target.result;
-    if (!thisDB.objectStoreNames.contains('data')) {
-        thisDB.createObjectStore('data', { autoIncrement: true });
-    }
+    console.log(`Request error: ${error}`);
 };
 
 request.onsuccess = function(e) {
-    console.log('Success!');
-    db = e.target.result;
-    console.log(db);
+    console.log(`Successful`);
+
+    var db = e.target.result;
+    var transaction = db.transaction(['data']);
+    var objectStore = transaction.objectStore('data');
+
+    objectStore.getAll().onsuccess = function(e) {
+        e.target.result.forEach(res => {
+            console.log(res.id);
+        });
+    };
 };
 
 // Service Worker
 const cacheName = 'v1';
 
 // Call Install Event
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
     console.log('Service Worker: Installed');
 });
 
 // Call Activate Event
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', e => {
     console.log('Service Worker: Activated');
 
     // Remove unwanted caches
     e.waitUntil(
-        caches.keys().then((cacheNames) => {
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.map((cache) => {
+                cacheNames.map(cache => {
                     if (cache !== cacheName) {
                         console.log('Service Worker: Clearing Old Cache');
                         return caches.delete(cache);
                     }
-                }),
+                })
             );
-        }),
+        })
     );
 });
 
 // Call Fetch Event
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
     console.log('Service Worker: Fetching');
     e.respondWith(
         fetch(e.request)
-            .then((res) => {
+            .then(res => {
                 // Make copy/clone of response
                 const resClone = res.clone();
 
                 // Open cache
-                caches.open(cacheName).then((cache) => {
+                caches.open(cacheName).then(cache => {
                     // Add response to cache
                     cache.put(e.request, resClone);
                 });
                 return res;
             })
-            .catch((err) => caches.match(e.request).then((res) => res)),
+            .catch(err => caches.match(e.request).then(res => res))
     );
 });
